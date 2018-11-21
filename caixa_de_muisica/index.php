45f31daed2e41fb71470b7a56d34b60f439d38ea
B1t-d3cod3r <45238461+B1t-d3cod3r@users.noreply.github.com>
2018-11-21 14:07:53 -0200

    Add files via upload

---
+++ caixa_de_muisica/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Caixa de musica</title>
+    <link rel="stylesheet" href="_css/index.css">
+  </head>
+  <body>
+    <header class="header">
+      <!-- BANNER -->
+      <div class="banner">
+        <!-- LOGIN ZONE -->
+        <form class="login-zone">
+          Nome de Usuário:<input type="text">
+          Senha:<input type="password">
+        </form>
+        <br><br><br><br>
+        <p>Coffee Break hacker</p>
+        <hr>
+      </div>
+      <hr>
+    </header>
+    <!-- AREA PRINCIPAL -->
+    <main class="main">
+
+      <!-- SOBRE -->
+      <div class="sobre-zone">
+        <p id="text1">Escute</p>
+        <p id="text2">Sinta</p>
+        <p id="text3">e Desfrute</p>
+      </div>
+
+      <!-- CADASTRO -->
+      <div class="cadastro-zone">
+        <form>
+          <fieldset><legend>Area de Cadastro</legend>
+            <p>Nome de Usuário:<input type="text"></p>
+            <hr>
+            <p>Senha:<input type="password"></p>
+            <hr>
+            <p>Confirme a Senha:<input type="password"></p>
+          </fieldset>
+        </form>
+      </div>
+    </main>
+  </body>
+</html>
